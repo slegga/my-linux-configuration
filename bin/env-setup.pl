54b194254f27b39121bf46e41c1e02535106095c
Stein Hammer <sslegga@gmail.com>
2017-11-07 22:15:49 +0100

    Add settings.json

--- bin/env-setup.pl
+++ bin/env-setup.pl
@@ -2,27 +2,45 @@
 use FindBin;
 use Mojo::Base -strict;
 use File::Copy 'copy';
+use autodie;
+
 # use lib "$FindBin::Bin/../lib";
 
+sub link_file {
+	my ($file, $link) = @_;
+	if (-l $link) {
+		return;
+	}
+	if ( -f $link ) {
+		copy($file, $link) or die "Cant copy($file, $link): $!";
+		return;
+	}
+	warn "link , $file, $link";
+	my $return = link $file, $link;
+	if (! $return ) {
+	 	my $err = $!;
+	 	if ($err =~ m/Invalid cross-device link/) {
+			 copy( $file, $link ) or die "Cant copy($file, $link): $!";
+	 	} else {
+			die "Cant make link $link $!"
+		}
+	}
+}
+
 my $gitdir = "$FindBin::Bin/../../";
-if (-d "$ENV{HOME}/.config/micro") {
+my $microdir = "$ENV{HOME}/.config/micro";
+my $repocnfdir = "$FindBin::Bin/../config";
+if ( -d $microdir ) {
+	my $settingfile = "$microdir/settings.json";
+	link_file( "$repocnfdir/micro/settings.json", $settingfile );
 	my $syntaxdir = "$ENV{HOME}/.config/micro/syntax";
 	if ( ! -d $syntaxdir ) {
 		warn "mkdir $syntaxdir";
 		mkdir $syntaxdir;
 	}
 	my $linkperlyaml = "$syntaxdir/perl.yaml";
-	if (! -l $linkperlyaml) {
+	if (! -f $linkperlyaml) {
 		my $repofile = "$gitdir/my-linux-configuration/config/micro/syntax/perl.yaml";
-		warn "link , $repofile, $linkperlyaml";
-		my $return = link $repofile, $linkperlyaml;
-		 if (! $return ) {
-		 	my $err = $!;
-		 	if ($err =~ m/Invalid cross-device link/) {
-				 copy( $repofile, $linkperlyaml ) or die "Cant copy($repofile, $linkperlyaml): $!";
-		 	} else {
-				die "Cant make link $linkperlyaml $!"
-			}
-		}
+		link_file($repofile, $linkperlyaml);
 	}
 }
