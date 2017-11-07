2eb9925d22fb9544b629d4a7905abd4ba2d427bc
slegga3 <sslegga@gmail.com>
2017-11-07 15:51:00 +0100

    Add config/micro/settings.json

--- bin/env-setup.pl
+++ bin/env-setup.pl
@@ -1,15 +1,28 @@
 #!/usr/bin/env perl
 use FindBin;
+use Mojo::Base -strict;
+use File::Copy 'copy';
 # use lib "$FindBin::Bin/../lib";
 
 my $gitdir = "$FindBin::Bin/../../";
 if (-d "$ENV{HOME}/.config/micro") {
 	my $syntaxdir = "$ENV{HOME}/.config/micro/syntax";
-	if (! -d $syntaxdir) {
+	if ( ! -d $syntaxdir ) {
+		warn "mkdir $syntaxdir";
 		mkdir $syntaxdir;
 	}
-	my $perlyaml = "$syntaxdir/perl.yaml";
-	if (! -f $perlyaml) {
-		link "$gitdir/my-linux-configuration/config/micro/syntax/perl.yaml", $perlyaml;
+	my $linkperlyaml = "$syntaxdir/perl.yaml";
+	if (! -l $linkperlyaml) {
+		my $repofile = "$gitdir/my-linux-configuration/config/micro/syntax/perl.yaml";
+		warn "link , $repofile, $linkperlyaml";
+		my $return = link $repofile, $linkperlyaml;
+		 if (! $return ) {
+		 	my $err = $!;
+		 	if ($err =~ m/Invalid cross-device link/) {
+				 copy( $repofile, $linkperlyaml ) or die "Cant copy($repofile, $linkperlyaml): $!";
+		 	} else {
+				die "Cant make link $linkperlyaml $!"
+			}
+		}
 	}
 }
