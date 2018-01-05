8b577d8eda65719aa6964c68c7e19dcfe9a2dd1a
Slegga <steihamm@gmail.com>
2018-01-05 14:32:33 +0100

    mer

---
+++ bash-functions.sh
@@ -0,0 +1,36 @@
+install () {
+    prove -l 2>&1 >/tmp/git-install.txt
+    SUCCESS=$(grep successful /tmp/git-install.txt)
+    if [ -z $SUCCESS ]; then
+        cat /tmp/git-install.txt
+    else
+        git push
+    fi
+}
+
+_cdg_complete () {
+  local cur prev
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  if [ $prev = "cdg" ]; then
+    COMPREPLY=( $(compgen -W "$(ls -d $HOME/git/*/ | cut -f5 -d'/')" -- $cur) );
+    return 0
+  fi
+}
+
+complete -F _cdg_complete cdg
+
+cdg () {
+    cd $HOME/git/$1;
+    git config credential.helper 'store'
+    git status >/tmp/git-status.txt
+    UMERGE=$(grep Unmerge /tmp/git-status.txt)
+    if [ -z $UNMERGE ]; then
+        git pull
+        prove -l
+    else
+        cat /tmp/git-status.txt
+   fi
+}
---
+++ my-bash-extra.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:~/bin
+alias m='micro'
+source ~/git/my-linux-configuration/bash-functions.sh
