8bf29fc7236509bb21edb1206bce4b9aa5786eb7
Stein Hammer <steihamm@gmail.com>
2020-11-12 11:36:58 +0100

    Fix error message with new shells. And add color less i hope

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -1,15 +1,17 @@
 #sudo apt install libsource-highlight-common source-highlight
 #dpkg -L libsource-highlight-common | grep lesspipe
-export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+if [ "$(command -v src-hilite-lesspipe.sh)" ]; then
+    export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+fi
 # sudo apt install exa
 alias rm="rm -v"
 alias less="less -R"
 alias reload="cd ~/ansible;ansible-playbook -i hosts update.yml -K -t mojo"
 if [ "$(command -v exa)" ]; then
-    unalias -m 'll'
-    unalias -m 'l'
-    unalias -m 'la'
-    unalias -m 'ls'
+#    unalias 'll'
+#    unalias -m 'l'
+#    unalias -m 'la'
+#    unalias 'ls'
     alias ls='exa -G  --color auto --icons -a -s type'
     alias ll='exa -l --color always --icons -a -s type'
 else
