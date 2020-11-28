c7220f5cb678403cd6d127741c6b6c33f7cbf6f3
Stein Hammer <steihamm@gmail.com>
2020-11-28 15:25:43 +0100

    Legger på lib=. for å få lagret unna debugsetup

--- my-bash-extra.sh
+++ my-bash-extra.sh
@@ -1,5 +1,5 @@
 export PATH=$PATH:~/bin
-alias perldebug='/usr/bin/env perl -S -d:ptkdb'
+alias perldebug='PERL5LIB=. /usr/bin/env perl -S -d:ptkdb'
 source ~/git/my-linux-configuration/bash-functions.sh
 source ~/git/my-linux-configuration/bash-aliases.sh
 
